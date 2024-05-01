a4aabd025bd36f27024ae43943a1cf5d0f230d85
Harsha vardhan <harshavardhan1617@gmail.com>
2024-05-01 13:45:14 +0530

    write functions for fetching user and repos data

--- api/index.js
+++ api/index.js
@@ -9,7 +9,24 @@ app.use(express.urlencoded({ extended: true }));
 const gitRequestRepos = async (usr) => {
   const res = await fetch(`https://api.github.com/users/${usr}/repos`);
   const data = await res.json();
-  return data;
+  if (Array.isArray(data)) {
+    const mapRepos = data.map((obj) => {
+      const { id, name, description, topics } = obj;
+      return { id, name, description, topics };
+    });
+    return mapRepos;
+  } else {
+    const { id, name, description, topics } = data;
+    return { id, name, description, topics };
+  }
+};
+
+const gitRequestUser = async (usr) => {
+  const res = await fetch(`https://api.github.com/users/${usr}`);
+  const body = await res.json();
+  const { id, avatar_url, name, location, bio, twitter_username } = body;
+  const user = { id, avatar_url, name, location, bio, twitter_username };
+  return user;
 };
 
 app.get("/:id", async (req, res) => {

5af7fc3443f218cf1de95f4f20fceefc72c59b56
Harsha vardhan <harshavardhan1617@gmail.com>
2024-05-01 13:28:43 +0530

    added github api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-app.get("/test", (req, res) => res.send("testing!!"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+
+const gitRequestRepos = async (usr) => {
+  const res = await fetch(`https://api.github.com/users/${usr}/repos`);
+  const data = await res.json();
+  return data;
+};
+
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const repos = await gitRequestRepos(`${id}`);
+  res.send(repos);
+});
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
